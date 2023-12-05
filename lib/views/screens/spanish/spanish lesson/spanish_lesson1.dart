f588f964b7b4522b6e200e075f5de22c6268b9d6
PasiMohit4 <pasimohit4@gmail.com>
2023-12-05 21:20:06 +0530

    Added Database Question Properly

---
+++ lib/views/screens/spanish/spanish lesson/spanish_lesson1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SpanishLesson1 extends StatefulWidget {
+  const SpanishLesson1({Key? key}) : super(key: key);
+
+  @override
+  State<SpanishLesson1> createState() => _SpanishLesson1State();
+}
+
+class _SpanishLesson1State extends State<SpanishLesson1> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('lesson 1'),
+    );
+  }
+}
---
+++ lib/views/screens/spanish/spanish lesson/spanish_lesson2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SpanishLesson2 extends StatefulWidget {
+  const SpanishLesson2({Key? key}) : super(key: key);
+
+  @override
+  State<SpanishLesson2> createState() => _SpanishLesson2State();
+}
+
+class _SpanishLesson2State extends State<SpanishLesson2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('lesson 2'),
+    );
+  }
+}
